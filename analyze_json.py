dc9fff2347268d59971c78eddf642f55e13646cd
K Fitzsimmons <kafitz22@gmail.com>
2021-11-02 18:25:06 -0400

    Create database script recursively fetches all transactions starting by a known start block or the last one observed in cache files; analyze JSON sums values based upon rawContract data due to empty `value` attributes for CADC (maybe others)

--- analyze_json.py
+++ analyze_json.py
@@ -2,17 +2,83 @@
 import json
 from pprint import pprint
 
+DIVISORS = {
+    'USDC': 1E6,
+    'CADC': 1E18,
+}
 
+
+# iterate over json file and generate a list of deposit transactions
 def process_deposits(currency_pair, request_id):
     cache_fp = f'{currency_pair}-deposits-{request_id}.json'
     with open(cache_fp, 'r') as cache_f:
         data = json.load(cache_f)
 
+    deposits = []
     for transfer in data['result']['transfers']:
-        print(transfer)
-        break
+        value = int(transfer['rawContract']['value'], 16) / DIVISORS[transfer['asset']]
+        deposits.append(
+            (transfer['from'], transfer['blockNum'], transfer['asset'], value)
+        )
+    return deposits
+
+
+# iterate over json file and generate a list of withdrawal transactions
+def process_withdrawals(currency_pair, request_id):
+    cache_fp = f'{currency_pair}-withdrawals-{request_id}.json'
+    with open(cache_fp, 'r') as cache_f:
+        data = json.load(cache_f)
+
+    withdrawals = []
+    for transfer in data['result']['transfers']:
+        value = int(transfer['rawContract']['value'], 16) / DIVISORS[transfer['asset']]
+        withdrawals.append(
+            (transfer['to'], transfer['blockNum'], transfer['asset'], value)
+        )
+    return withdrawals
+
+
+# sum list of formatted transfers
+def total(transfers):
+    balance = {}
+    for _, _, asset, value in transfers:
+        balance.setdefault(asset, 0)
+        if value:
+            balance[asset] += float(value)
+    return balance
 
 
 if __name__ == '__main__':
-    process_deposits('TRYB-USDC', request_id=0)
+    ## TRYB / USDC
+    ## 
+    # deposits = process_deposits('TRYB-USDC', request_id=0)
+    # withdrawals = process_withdrawals('TRYB-USDC', request_id=0)
+
+    # total_deposits = total(deposits)
+    # print('deposits:')
+    # print(total_deposits)
+
+    # total_withdrawals = total(withdrawals)
+    # print('withdrawals:')
+    # print(total_withdrawals)
+
+    # print('net (USDC):', total_deposits['USDC'] - total_withdrawals['USDC'])
+    # print('net (TRYB):', total_deposits['TRYB'] - total_withdrawals['TRYB'])
+
+    ## CADC / USDC
+    ## 
+    deposits = process_deposits('CADC-USDC', request_id=0)
+    withdrawals = process_withdrawals('CADC-USDC', request_id=0)
+
+    total_deposits = total(deposits)
+    print('deposits:')
+    print(total_deposits)
+
+    total_withdrawals = total(withdrawals)
+    print('withdrawals:')
+    print(total_withdrawals)
+
+    print('net (USDC):', total_deposits['USDC'] - total_withdrawals['USDC'])
+    print('net (CADC):', total_deposits['CADC'] - total_withdrawals['CADC'])
+
 
--- create_db.py
+++ create_db.py
@@ -3,6 +3,7 @@ import dotenv
 import json
 import requests
 import os
+import time
 
 dotenv.load_dotenv()
 
@@ -11,18 +12,18 @@ ALCHEMY_BASE_URL = f'https://eth-mainnet.alchemyapi.io/v2/{ALCHEMY_API_KEY}'
 
 
 POOL_CONTRACTS = {
-    'CADC-USDC': '0xa6C0CbCaebd93AD3C6c94412EC06aaA37870216d',
-    'EURS-USDC': '0x1a4Ffe0DCbDB4d551cfcA61A5626aFD190731347',
-    'XSGD-USDC': '0x2baB29a12a9527a179Da88F422cDaaA223A90bD5',
-    'NZDS-USDC': '0xe9669516e09f5710023566458f329cce6437aaac',
-    'TRYB-USDC': '0xc574a613a3900e4314da13eb2287f13689a5b64d',
+    'CADC-USDC': ('0xa6C0CbCaebd93AD3C6c94412EC06aaA37870216d', 12459301),
+    'EURS-USDC': ('0x1a4Ffe0DCbDB4d551cfcA61A5626aFD190731347', 12459337),
+    'XSGD-USDC': ('0x2baB29a12a9527a179Da88F422cDaaA223A90bD5', 12459348),
+    'NZDS-USDC': ('0xe9669516e09f5710023566458f329cce6437aaac', 13283851),
+    'TRYB-USDC': ('0xc574a613a3900e4314da13eb2287f13689a5b64d', 13234461),
 }
 STAKING_CONTRACTS = {
-    'CADC-USDC': '0x84Bf8151394dcF32146965753B28760550f3D7A8',
-    'EURS-USDC': '0x5EaAEff69f2aB64d1CC0244FB31B236cA989544f',
-    'XSGD-USDC': '0xd52D48Db08e8224ef6E2be8F54f3c84e790b1c32',
-    'NZDS-USDC': '0xe06FA52e0d2D58Fe192285bfa0507F09cDd9824a',
-    'TRYB-USDC': '0xdDB720069fdfE7BE2E2883A1c06BE0f353f7C4c8',    
+    'CADC-USDC': ('0x84Bf8151394dcF32146965753B28760550f3D7A8', 12459404),
+    'EURS-USDC': ('0x5EaAEff69f2aB64d1CC0244FB31B236cA989544f', 12459411),
+    'XSGD-USDC': ('0xd52D48Db08e8224ef6E2be8F54f3c84e790b1c32', 12459420),
+    'NZDS-USDC': ('0xe06FA52e0d2D58Fe192285bfa0507F09cDd9824a', 13232886),
+    'TRYB-USDC': ('0xdDB720069fdfE7BE2E2883A1c06BE0f353f7C4c8', 13282257),
 }
 
 
@@ -39,8 +40,8 @@ def get_gas_price():
 
 
 
-def get_asset_transfers(currency_pair, deposits=True, request_id=0):
-    label = 'deposits' if deposits else 'withdraws'
+def get_asset_transfers(currency_pair, contract_address, start_block, request_id=0, deposits=True):
+    label = 'deposits' if deposits else 'withdrawals'
     cache_fp = f'{currency_pair}-{label}-{request_id}.json'
     if not os.path.exists(cache_fp):
         params = {
@@ -48,9 +49,8 @@ def get_asset_transfers(currency_pair, deposits=True, request_id=0):
             'method': 'alchemy_getAssetTransfers',
             'id': request_id,
             'params': [{
-                'fromBlock': '0xCAB836',
-                # 'toBlock': '0xCAB840',
-                'maxCount': '0x3E8',
+                'fromBlock': hex(start_block),
+                'maxCount': hex(1000),
                 'excludeZeroValue': True,
                 'category': [
                     'external',
@@ -59,16 +59,18 @@ def get_asset_transfers(currency_pair, deposits=True, request_id=0):
             }]
         }
         if deposits:
-            params['params'][0]['toAddress'] = POOL_CONTRACTS[currency_pair]
+            params['params'][0]['toAddress'] = contract_address
         else:
-            params['params'][0]['fromAddress'] = POOL_CONTRACTS[currency_pair]
+            params['params'][0]['fromAddress'] = contract_address
 
 
         r = requests.post(ALCHEMY_BASE_URL, json=params)
         data = r.json()
 
-        with open(cache_fp, 'w') as cache_f:
-            json.dump(r.json(), cache_f)
+        # only cache when data exists
+        if 'result' in data and len(data['result']['transfers']) != 0:
+            with open(cache_fp, 'w') as cache_f:
+                json.dump(r.json(), cache_f)
     else:
         with open(cache_fp, 'r') as cache_f:
             data = json.load(cache_f)
@@ -76,8 +78,67 @@ def get_asset_transfers(currency_pair, deposits=True, request_id=0):
     return data
 
 
+def _get_or_init_request_idx(currency_pair, tx_type):
+    new_idx = 0
+    for fn in sorted(os.listdir()):
+        if currency_pair in fn:
+            base_fn_parts = fn.rstrip('.json').split('-')
+            if base_fn_parts[2] != tx_type:
+                continue
+            new_idx = int(base_fn_parts[-1]) + 1
+    return new_idx
+
+def _get_next_block_num(transfers):
+    return int(transfers[-1]['blockNum'], 16) + 1
+
+
+def _load_next_block_from_file(currency_pair, tx_type, request_id):
+    cache_fp = f'{currency_pair}-{tx_type}-{request_id}.json'
+    with open(cache_fp, 'r') as cache_f:
+        data = json.load(cache_f)
+    next_block = _get_next_block_num(data['result']['transfers'])
+    return next_block
+
+
+# run fetch request in loop based upon latest block number until no new transactions exist
+def fetch_all_data(currency_pair, contract_info, deposits=True):
+    contract_address, start_block = contract_info
+    tx_type = 'deposits' if deposits is True else 'withdrawals'
+    
+    print(f'Fetching {tx_type} for {currency_pair}...')
+
+    # initialize request id and start block from 0 or from existing data
+    request_idx = _get_or_init_request_idx(currency_pair, tx_type)
+    if request_idx != 0:
+        start_block = _load_next_block_from_file(currency_pair, tx_type, request_idx - 1)
+
+    first_request = True
+    transfers_data = None
+    while True:
+        # sleep between consecutive requests and get next start block
+        # from last seen block num + 1
+        if not first_request:
+            time.sleep(1)
+            start_block = _get_next_block_num(transfers_data)
+
+        # perform API requests and break when new data is empty
+        print('Start block num:', start_block)
+        data = get_asset_transfers(
+            currency_pair,
+            contract_address, 
+            start_block, 
+            request_idx,
+            deposits=deposits)
+        if 'result' not in data or len(data['result']['transfers']) == 0:
+            break
+        transfers_data = data['result']['transfers']
+        print('Results:', len(transfers_data))
+        request_idx += 1
+        first_request = False
+
 
 if __name__ == '__main__':
-    deposits_data = get_asset_transfers('TRYB-USDC', deposits=True, request_id=0)
-    deposits_data = get_asset_transfers('TRYB-USDC', deposits=False, request_id=0)
-
+    for currency_pair, contract_info in POOL_CONTRACTS.items():
+        fetch_all_data(currency_pair, contract_info, deposits=True)
+        fetch_all_data(currency_pair, contract_info, deposits=False)
+        # break
